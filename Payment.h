31c6edad3d94f737cb1bba4f6ae3d3c70c71cc60
Namal Jayawardana <105379668+IT21291746@users.noreply.github.com>
2022-05-20 03:04:53 +0530

    Add files via upload

---
+++ Payment.h
@@ -0,0 +1,19 @@
+//IT21291746 MADUSHANKA J G N 
+#include "Busticket.h"
+//paymet class
+class Payment{
+  private:
+    int paymentID;
+    char cardType[70];
+    double amount;
+    int cardNo;
+    int cvvNo;
+    char cardExpDate[20];
+    Busticket *busticket;   //Composition Relationship
+  public:
+    Payment();
+    Payment(int paymentID,char cType[],double total,int cardNo,int cvvNo,char cardExp[]);
+    void setPaymentDetails();
+    void printPaymentDetails();
+    ~Payment();
+};
