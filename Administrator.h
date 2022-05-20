43b732cb051d1339874e644c8fc18f44220c33fd
IT21344510 <100065668+IT21344510@users.noreply.github.com>
2022-05-20 13:00:22 +0530

    Create Administrator.h

---
+++ Administrator.h
@@ -0,0 +1,13 @@
+class Administrator{
+	private:
+		int adminID;
+		char adminName;
+		int adminContactNo;
+		char adminEmail;
+		
+	public:
+        Administrator();
+	  	Administrator(int aID,char aName,int aContactNo , char aEmail);       
+		void setadminDetails(int aID,char aName,int aContactNo, char aEmail);
+		void displayadminDetails();
+  		~Administrator ();
